c81b38da9f390a381792619ab736b8db5bce98a1
rhenzo pacho <khrandm@gmail.com>
2020-02-19 23:50:36 +0800

    Updates

---
+++ precache-manifest.d278708f25184fe895e5c99ff71cb7b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad962b321eff1f9c9561d709e3eba3c",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e6ca775ec02f43e8b1db",
+    "url": "/calculator/static/js/2.b8cf4acc.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/calculator/static/js/2.b8cf4acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f103b2d3906c422a44",
+    "url": "/calculator/static/js/main.53168dde.chunk.js"
+  },
+  {
+    "revision": "e73c141eb177922455a3",
+    "url": "/calculator/static/js/runtime-main.ccdab4cd.js"
+  }
+]);
